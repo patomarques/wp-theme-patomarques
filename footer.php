373786a104423e341fea32fd931a0acfb07d8635
Pato Marques <patoxmarques@gmail.com>
2023-02-13 16:38:31 -0300

    home custom

--- footer.php
+++ footer.php
@@ -1,18 +1,5 @@
-			<?php
-				// If Single or Archive (Category, Tag, Author or a Date based page).
-				if ( is_single() || is_archive() ) :
-			?>
-					</div><!-- /.col -->
+		</main>
 
-					<?php
-						get_sidebar();
-					?>
-
-				</div><!-- /.row -->
-			<?php
-				endif;
-			?>
-		</main><!-- /#main -->
 		<footer id="footer">
 			<div class="container">
 				<div class="row">
--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 
 <div id="wrapper">
 	<header>
-		<nav id="header" class="navbar navbar-expand-md <?php echo esc_attr( $navbar_scheme ); if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom'; endif; if ( is_home() || is_front_page() ) : echo ' home'; endif; ?>">
+		<nav id="header" class="navbar navbar-expand-xs <?php echo esc_attr( $navbar_scheme ); if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom'; endif; if ( is_home() || is_front_page() ) : echo ' home'; endif; ?>">
 			<div class="container">
 				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php
@@ -47,7 +47,6 @@
 
 				<div id="navbar" class="collapse navbar-collapse">
 					<?php
-						// Loading WordPress Custom Menu (theme_location).
 						wp_nav_menu(
 							array(
 								'menu_class'     => 'navbar-nav me-auto',
@@ -74,13 +73,13 @@
 		</nav>
 	</header>
 
-	<main id="main" class="container"<?php if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' style="padding-top: 100px;"'; elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' style="padding-bottom: 100px;"'; endif; ?>>
-		<?php
-			// If Single or Archive (Category, Tag, Author or a Date based page).
-			if ( is_single() || is_archive() ) :
-		?>
-			<div class="row">
-				<div class="col-12">
-		<?php
-			endif;
-		?>
+  <?php
+    $navbarMargin = '';
+    if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ){
+      $navbarMargin = 'pt-100';
+    } elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) {
+      $navbarMargin = 'pb-100';
+    }
+  ?>
+
+	<main id="main" class="<?= $navbarMargin ?>">
