4fd3ec2449213297fc2b04f3fd8d4be356047811
Pato Marques <patoxmarques@gmail.com>
2023-02-14 23:30:17 -0300

    assets: full screen hamburger menu has been implemented

---
+++ assets/js/wp-patomarques.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-hamburguer').addEventListener('click', hiddenHeader);
+
+function hiddenHeader() {
+  console.log('menu has been clicked');
+  // document.querySelector('header').classList.toggle('hidden');
+}
