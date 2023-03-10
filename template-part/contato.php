da2728410da5b93e177798ff79a020767dbe71f7
Pato Marques <patoxmarques@gmail.com>
2023-03-10 13:16:29 -0300

    home > contact has been developed

--- template-part/contato.php
+++ template-part/contato.php
@@ -7,7 +7,7 @@
 
 <section class="container-fluid section">
   <div class="container">
-    <div class="row">
+    <div class="row text-center hidden">
       <div class="col-12">
         <h3 class="title">
           <?= $page->post_title ?>
@@ -15,14 +15,23 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
-        <?php while ( $contatos->have_posts() ) : $contatos->the_post(); ?>
-          <a href="#" alt="<?= get_the_title() ?>" title="<?= get_the_title() ?>" rel="noopener noreferrer">
-            <span class="icon"><?= get_the_title() ?></span>
-          </a>
-        <?php endwhile;
-          wp_reset_postdata();
-        ?>
+      <div class="col-12 text-center">
+        <ul class="list-inline list-unstyled contact-list">
+
+          <?php while ( $contatos->have_posts() ) : $contatos->the_post(); ?>
+
+            <li class="list-inline-item contact-list__item">
+              <a href="<?= get_post_meta(get_the_ID(), 'link', true) ?>" alt="<?= get_the_title() ?>"
+              title="<?= get_the_title() ?>" rel="noopener noreferrer" class="contact-list__link">
+                <i class="<?= get_post_meta(get_the_ID(), 'icon-class', true) ?>"></i>
+              </a>
+            </li>
+
+          <?php endwhile;
+            wp_reset_postdata();
+          ?>
+
+        </ul>
       </div>
     </div>
   </div>
