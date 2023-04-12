6a14055aeded8f92c534f282e35650e4be392535
Pato Marques <patoxmarques@gmail.com>
2023-04-12 17:17:56 -0300

    skill sections was changed

--- template-part/habilidades.php
+++ template-part/habilidades.php
@@ -1,7 +1,7 @@
 <?php
   $page = get_page_by_path('habilidades');
 
-  $args = array( 'post_type' => 'habilidades', 'posts_per_page' => -1 );
+  $args = array( 'post_type' => 'habilidades', 'posts_per_page' => -1, 'orderby' => 'order', 'order' => 'ASC' );
   $skills = new WP_Query( $args );
 ?>
 <section class="container-fluid section">
@@ -14,30 +14,52 @@
     <div class="row">
       <div class="col-12">
         <p class="content-text">
-
-
-
           <?= $page->post_content ?>
         </p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-12 col-md-6 content-skills">
 
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <div class="row content-skills">
+
+          <?php while ( $skills->have_posts() ) : $skills->the_post(); ?>
+
+          <?php if(get_post_meta(get_the_ID(), 'category-graph', true) == 'bar'){ ?>
+
+            <div class="col-12 col-md-6 content-skills__bars">
+              <div class="content-skills__line">
+                <div class="text-center"><?= get_the_title() ?></div>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated progress-bar-custom" role="progressbar" aria-label="Basic example" style="width: <?= get_post_meta(get_the_ID(), 'percent', true) ?>%" aria-valuenow="<?= get_post_meta(get_the_ID(), 'percent', true) ?>" aria-valuemin="0" aria-valuemax="100"><?= get_post_meta(get_the_ID(), 'percent', true) ?>%</div>
+                </div>
+              </div>
+            </div>
+
+          <?php } ?>
+
+          <?php
+            endwhile;
+            wp_reset_postdata();
+          ?>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
         <?php while ( $skills->have_posts() ) : $skills->the_post(); ?>
 
-        <div class="content-skills__line">
-          <div class="text-center"><?= get_the_title() ?></div>
-          <div class="progress">
-            <div class="progress-bar progress-bar-striped progress-bar-animated progress-bar-custom" role="progressbar" aria-label="Basic example" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
-          </div>
-        </div>
+        <?php if(get_post_meta(get_the_ID(), 'category-graph', true) == 'circle'){ ?>
 
-         <?php
-          endwhile;
-          wp_reset_postdata();
-        ?>
+          <?= get_the_title() ?>
+          <?= get_post_meta(get_the_ID(), 'percent', true) ?>
+
+        <?php } ?>
+
+        <?php
+            endwhile;
+            wp_reset_postdata();
+          ?>
       </div>
     </div>
+
   </div>
 </section>
